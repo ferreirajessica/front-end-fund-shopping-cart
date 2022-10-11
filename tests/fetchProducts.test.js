ce5a5604a409de348ea0d207e9457d01a9292e44
Jessica Ferreira <samojess@gmail.com>
2022-10-11 16:24:04 -0300

    finaliza requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('tests whether fetchProducts is a function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('tests whether fetch has been called', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+  it('tests fetch endpoint to be https://api.mercadolibre.com/sites/MLB/search?q=computador', async () => {
+     await fetchProducts('computador');
+     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it(`tests fetchProducts return to be ${computadorSearch}`, async () => {
+    expect(fetchProducts('computador')).resolves.toEqual(`${computadorSearch}`);
+  });
+  it('returns error when there are no arguments', async () => {
+    expect(fetchProducts()).toThrow('You must provide an url');
+  })
 });
+
