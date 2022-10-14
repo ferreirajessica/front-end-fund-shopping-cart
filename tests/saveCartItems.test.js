167625b1581fe69e45d02cb926d0cc2560fcb85a
Jessica Ferreira <samojess@gmail.com>
2022-10-14 15:50:04 -0300

    finaliza requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
-});
+  it('saves items to local storage', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('is called with arguments cartItems and saveCartItems', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', cartItem);
+  });
+ });
